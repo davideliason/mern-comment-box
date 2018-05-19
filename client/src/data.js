4a57083065b7c094cf709308fc6aad198cb88fc0
David Eliason <david@davethemaker.com>
2018-05-19 14:37:52 -0700

    build out Comment component which receives props of author and id and children and displays them using react-markdown for convenience for simple text styling

--- client/src/data.js
+++ client/src/data.js
@@ -1,3 +1,7 @@
-const data = "hello there i \'m data";
+const data = [
+  { id: 1, author: 'David', text: '### I need some coffee', updatedAt: new Date() },
+  { id: 2, author: 'You', text: 'You\'re so very right!', updatedAt: new Date() },
+];
 
 export default data;
+
