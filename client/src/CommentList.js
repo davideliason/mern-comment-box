a43b9ac76c81e2fcb53a41e05191447605d79815
David Eliason <david@davethemaker.com>
2018-05-19 14:21:26 -0700

    pass in props.data to CommentList component from CommentBox, with the text key field data being set to children

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,18 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
 
-class CommentList extends Component {
-	constructor(){
-		super();
-		this.state={
-			color: "blue"
-		}
-	}
+const CommentList = (props) => {
+  const commentNodes = props.data.map(comment => (
+    <Comment author={comment.author} key={comment._id} id={comment._id}>
+      { comment.text}
+    </Comment>
+  ));
+  return (
+    <div>
+      { commentNodes }
+    </div>
+  );
+};
 
-	render(){
-		return (
-				<div> CommentList {this.props.data}</div>
-			)
-	}
-}
+CommentList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    author: PropTypes.string,
+    id: PropTypes.string,
+    text: PropTypes.string,
+  })),
+};
+
+CommentList.defaultProps = {
+  data: [],
+};
 
 export default CommentList;
