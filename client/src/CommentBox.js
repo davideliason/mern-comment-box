128948110a80d757c3e82543bbccb5c80d072d0a
David Eliason <david@davethemaker.com>
2018-05-19 14:00:33 -0700

    add CommentBox component which pulls in child components of CommentList and CommentForm

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -1,20 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import CommentList from './CommentList';
+import CommentForm from './CommentForm';
+import DATA from './data';
+import './CommentBox.css';
 
 class CommentBox extends Component {
-	constructor(){
-		super();
-		this.state = {
-			color: "blue"
-		}
-	}
-
-	render(){
-		return (
-				<div>
-					<h3>hello CommentBox</h3>
-				</div>
-			)
-	}
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="comments">
+          <h2>Comments:</h2>
+          <CommentList data={DATA} />
+        </div>
+        <div className="form">
+          <CommentForm />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default CommentBox
+export default CommentBox;
